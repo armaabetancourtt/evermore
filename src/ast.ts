728c230cf3f0546ab297e624bac65be94ac7da49
Armando Betancourt <armabetancourtt@gmail.com>
2026-09-22 11:50:32 -0600

    feat(compiler): define initial Evermore AST

---
+++ src/ast.ts
@@ -0,0 +1,45 @@
+export type SourcePosition = {
+  readonly offset: number;
+  readonly line: number;
+  readonly column: number;
+};
+
+export type SourceSpan = {
+  readonly start: SourcePosition;
+  readonly end: SourcePosition;
+};
+
+export type Program = {
+  readonly kind: "Program";
+  readonly appName: string;
+  readonly screens: readonly ScreenDeclaration[];
+  readonly span: SourceSpan;
+};
+
+export type ScreenDeclaration = {
+  readonly kind: "ScreenDeclaration";
+  readonly name: string;
+  readonly body: readonly UIStatement[];
+  readonly span: SourceSpan;
+};
+
+export type UIStatement = TitleStatement | ButtonStatement;
+
+export type TitleStatement = {
+  readonly kind: "TitleStatement";
+  readonly text: string;
+  readonly span: SourceSpan;
+};
+
+export type ButtonStatement = {
+  readonly kind: "ButtonStatement";
+  readonly label: string;
+  readonly action?: NavigationAction;
+  readonly span: SourceSpan;
+};
+
+export type NavigationAction = {
+  readonly kind: "NavigationAction";
+  readonly target: string;
+  readonly span: SourceSpan;
+};

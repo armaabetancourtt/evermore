3e0db9aaa5ef1e3ec069748e6e2dadbfd4a9f06b
Armando Betancourt <armabetancourtt@gmail.com>
2026-09-22 11:51:57 -0600

    feat: expose compiler API

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./ast.js";
+export * from "./compiler.js";
+export * from "./diagnostics.js";
+export * from "./lexer.js";
+export * from "./parser.js";
+export * from "./semantic.js";
